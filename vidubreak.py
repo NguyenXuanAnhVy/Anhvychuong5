30bd5a29d9d0798007257cbade06bda3293eb93d
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-09-12 19:21:21 +0700

    Add files via upload

---
+++ vidubreak.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 19:17:37 2024
+
+@author: Student
+"""
+
+tup= ("do", "cam", "vang", "luc", "lam", "cham", "tim")
+for i in tup:
+    print(i, "\t")
+    if i == "do":
+        break
+
