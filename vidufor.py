a2c68708135bc337004c9b54f03debb9421e9cf6
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-09-12 19:17:29 +0700

    Add files via upload

---
+++ vidufor.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 18:04:40 2024
+
+@author: anhvy
+"""
+#for in
+mau = ["do", "cam", "vang", "luc", "lam", "cham", "tim"]
+for i in mau:
+    print(i, "\t", len(i))
+
+#for in range(1)
+for i in range(10):
+    print(i, end='\t')
+    
+#for in range(2)
+for i in range(1, 10, 2):
+    print(i, end='\t')
