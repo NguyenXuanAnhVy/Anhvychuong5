fdb0152bfadd23f54c531fd6d101f4bbc3b591aa
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-09-13 13:33:03 +0700

    Add files via upload

---
+++ bangcuuchuong.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 13:26:48 2024
+
+@author: 
+"""
+for i in range(2, 10):
+    print("Bang cuu chuong " + str(i) + "\t")
+    for j in range(1, 11):
+        ketqua = i*j
+        print(str(i) + "x" + str(j) + "=" + str(ketqua))
+    print()
