a2c55aa6e2528d963fee7db97e47741f14543cb7
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-09-13 13:15:32 +0700

    Add files via upload

---
+++ giaithua.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 13:06:07 2024
+
+@author: anhvy 
+"""
+
+n= int(input("Nhap so n: "))
+giaithua= 1
+for i in range(1, n+1):
+    giaithua*= n
+print("Ket qua giai thua: ", giaithua)
